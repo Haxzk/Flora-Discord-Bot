ec12f37c8321b16bfaaa43a9ecff9d01c3ffe5b1
Haxzk <43327130+Haxzk@users.noreply.github.com>
2018-09-18 15:47:42 -0300

    Update AutoRegister.java

--- src/main/java/io/github/haxzk/flora/bot/libraries/AutoRegister.java
+++ src/main/java/io/github/haxzk/flora/bot/libraries/AutoRegister.java
@@ -10,18 +10,26 @@ public class AutoRegister {
     private Reflections reflections;
     private Set<Class<? extends Command>> commandClassSet;
     private Set<Class<? extends ListenerAdapter>> listenerClassSet;
+    private Set<Class<?>> classToRegister;
 
     public AutoRegister(String pack) {
-        reflections = new Reflections(pack);
-        commandClassSet = reflections.getSubTypesOf(Command.class);
-        listenerClassSet = reflections.getSubTypesOf(ListenerAdapter.class);
+        this.reflections = new Reflections(pack);
+        this.commandClassSet = reflections.getSubTypesOf(Command.class);
+        this.listenerClassSet = reflections.getSubTypesOf(ListenerAdapter.class);
+        this.classToRegister = new HashSet<>();
+        this.classToRegister.add(this.commandClassSet);
+        this.classToRegister.add(this.listenerClassSet);
     }
 
     public Set<Class<? extends Command>> getCommandClass() {
-        return commandClassSet;
+        return this.commandClassSet;
     }
 
     public Set<Class<? extends ListenerAdapter>> getListenerClass() {
-        return listenerClassSet;
+        return this.listenerClassSet;
+    }
+    
+    public Set<Class<?>> getClassToRegister() {
+        return this.classToRegister;
     }
 }
