a2673d6b68d74d38060200ec4728c5a7828658ed
Haxzk <eikefabricio545@gmail.com>
2018-09-20 18:15:08 -0300

    Update pom.xml

--- src/main/java/io/github/haxzk/flora/bot/FloraIA.java
+++ src/main/java/io/github/haxzk/flora/bot/FloraIA.java
@@ -5,7 +5,6 @@ import io.github.haxzk.flora.bot.libraries.Command;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
-import net.dv8tion.jda.core.entities.Game;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
@@ -18,9 +17,17 @@ public class FloraIA {
     private FloraIA() {
         try {
             jdaBuilderObject = new JDABuilder(AccountType.BOT).setToken("NDkxMjUzMzAxMDc4MzkyODMz.DoG8aA.lIXdcKhicHXjX5v85HiQsX6ufPY");
-            for (Class<? extends Command> c : new AutoRegister("io.github.haxzk.flora.bot").getCommandClass()) {
-                jdaBuilderObject.addEventListener(c.newInstance());
-                System.out.println("Comando : " + c.getName() + " registrado! ");
+            if (!new AutoRegister("io.github.haxzk.flora.bot").getCommandClass().isEmpty()) {
+                for (Class<? extends Command> c : new AutoRegister("io.github.haxzk.flora.bot").getCommandClass()) {
+                    jdaBuilderObject.addEventListener(c.newInstance());
+                    System.out.println("Comando : " + c.getName() + " registrado! ");
+                }
+            }
+            if (!new AutoRegister("io.github.haxzk.flora.bot").getListenerClass().isEmpty()) {
+                for (Class<? extends ListenerAdapter> c : new AutoRegister("io.github.haxzk.flora.bot").getListenerClass()) {
+                    jdaBuilderObject.addEventListener(c);
+                    System.out.println("Evento : " + c.getName() + " registrado! ");
+                }
             }
             jdaObject = jdaBuilderObject.build();
         } catch (LoginException e) {
